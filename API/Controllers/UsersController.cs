b171bcacf7603215eb1cbf80b6f11cf4a323781f
Arif Elahi <arifelahi@live.com>
2023-09-29 17:20:20 +0530

    end of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,31 +1,43 @@
-﻿using API.Data;
-using API.Entities;
+﻿using API.DTOs;
+using API.interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 [Authorize]
 public class UsersController : BaseApiController
 {
-    private readonly DataContext _context;
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
 
-    public UsersController(DataContext context)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
-        _context = context;
+        _userRepository = userRepository;
+        _mapper = mapper;
     }
 
-    [AllowAnonymous]
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        return await _context.Users.ToListAsync();
+        // var users = await _userRepository.GetUsersAsync();
+        // var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+        var users=await _userRepository.GetMembersAsync();
+        return Ok(users);
     }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>>GetUser(int id)
+    [HttpGet("{username}")]
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        return await _context.Users.FindAsync(id);
+        //this solution also works but read all data columns from both sql tables.
+        //automapper is used to refine the columns we need to provide to API output.
+
+        // var user= await _userRepository.GetUserByUsernameAsync(username);
+        // return _mapper.Map<MemberDto>(user);
+
+        //this method will return only required columns using automapper nuget package.
+        return await _userRepository.GetMemberAsync(username);
+
     }
 
 
---
+++ API/Extenstions/DateTimeExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Extentions;
+public static class DateTimeExtension
+{   
+    public static int CalculateAge(this DateOnly Dob)
+    {
+        var today=DateOnly.FromDateTime(DateTime.UtcNow);
+
+        var age=today.Year-Dob.Year;
+
+        if(Dob>today.AddYears(-age)) age--;
+
+        return age;
+    }
+}
